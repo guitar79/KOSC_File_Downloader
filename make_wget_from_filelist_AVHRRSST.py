101e552af4bb246b18d845be86e1c78a21aa7efb
Kiehyun Kevin Park <guitar79@naver.com>
2020-11-25 16:09:32 +0900

    Add files via upload

---
+++ make_wget_from_filelist_AVHRRSST.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 12:56:43 2020
+
+@author: guitar79
+
+# Open the file with read only permit
+
+</nfsdb/COMS/GOCI/1.2/2011/05/04/L2/COMS_GOCI_L2A_GA_20110504041642.CHL.he5.zip>
+http://222.236.46.45/nfsdb/COMS/GOCI/1.2/2011/11/04/L2/COMS_GOCI_L2A_GA_20111104021640.CHL.he5.zip
+http://222.236.46.45/nfsdb/COMS/GOCI/1.2/2011/05/04/L2/COMS_GOCI_L2A_GA_20110504041642.CHL.he5.zip
+http://222.236.46.45/nfsdb/NOAA/2012/07/24/L2/2012.0724.0342.noaa-19.sst.asc.zip
+
+"""
+
+#import os
+#dir_name = "../L2_SST_MODIS/"
+
+#filename_lst = sorted(os.listdir(dir_name))
+
+file_name = "MODIS_aqua_SST_filenames.txt"
+file_name = "GOCI_CHL_filenames.txt"
+file_name = "AVHRR_SST_filenames.txt"
+
+f = open("{}".format(file_name), "r")
+# use readlines to read all lines in the file
+# The variable "lines" is a list containing all lines in the file
+lines = f.readlines()
+# close the file after reading the lines.
+f.close()
+
+print("len(lines) :{}".format(len(lines)))
+
+url1 = "wget -T 300 -t 1 -r -nd -np -l 1 -N --no-if-modified-since -P ./  http://222.236.46.45/nfsdb/"
+url2 = "NOAA/"
+urls = ""
+level = "L2/"
+for line in lines[:] :
+    line = line.rstrip()
+    print("line :{}".format(line))
+    print("line[-4] :{}".format(line[-4]))
+    if line[-4:] == ".zip" :
+        filename_el = line.split(".")
+        #if filename_el[1] == "GOCI" : url2 = "COMS/GOCI/2.0/"
+        urls += "{0}{1}{2}/{3}/{4}/L2/{5}\n".\
+            format(url1, url2, \
+            filename_el[0], filename_el[1][:2], filename_el[1][2:], line)
+
+        '''
+        urls += "{0}{1}{2}/{3}/{4}/{7}{8}\n".\
+            format(url1, url2, filename_el[1],\
+                   filename_el[2][:2], filename_el[2][2:],\
+                   filename_el[3][2:], filename_el[3][:2],\
+                       level, line)
+        '''
+        print(urls)
+
+with open("{}_wget.sh".format(file_name[:-14]), 'w') as f2:
+	f2.write(urls)
